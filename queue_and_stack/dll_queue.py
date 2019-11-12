5d38f41cc1ec9ad179ebeed9c60a2e9f98b7fa1f
Laura Daugherty <lauradaugherty6@gmail.com>
2019-11-11 16:40:55 -0800

    instantiating DLL

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -5,6 +5,7 @@ from doubly_linked_list import DoublyLinkedList
 
 class Queue:
     def __init__(self):
+        self.storage = DoublyLinkedList()
         self.size = 0
         # Why is our DLL a good choice to store our elements?
         # self.storage = ?
