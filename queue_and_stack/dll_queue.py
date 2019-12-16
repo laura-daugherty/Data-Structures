2af8352b91cda8199a1b5fa855e24ae4ec247a83
Laura Daugherty <lauradaugherty6@gmail.com>
2019-12-16 12:35:10 -0800

    queue and stack reset

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -5,22 +5,15 @@ from doubly_linked_list import DoublyLinkedList
 
 class Queue:
     def __init__(self):
-        self.storage = DoublyLinkedList()
         self.size = 0
         # Why is our DLL a good choice to store our elements?
+        # self.storage = ?
 
     def enqueue(self, value):
-        self.storage.add_to_tail(value)
-        self.size +=1
-    
+        pass
 
     def dequeue(self):
-        if self.size == 0:
-            return None
-        else:
-            self.size -=1
-            return self.storage.remove_from_head()
-
+        pass
 
     def len(self):
-        return self.size
+        pass
--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -1,25 +1,19 @@
-from doubly_linked_list import DoublyLinkedList
+Learn more or give us feedback
 import sys
 sys.path.append('../doubly_linked_list')
-
+from doubly_linked_list import DoublyLinkedList
 
 class Stack:
     def __init__(self):
-        self.storage = DoublyLinkedList()
-
         self.size = 0
         # Why is our DLL a good choice to store our elements?
+        # self.storage = ?
 
     def push(self, value):
-        self.size += 1
-        self.storage.add_to_tail(value)
+        pass
 
     def pop(self):
-        if self.size > 0:
-            self.size -= 1
-            return self.storage.remove_from_tail()
-        else: 
-            return
+        pass
 
     def len(self):
-        return self.size
+        pass
